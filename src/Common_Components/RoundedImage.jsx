0f704fbb8f39368d4a1fc5b36a14be077fa1f2b8
SanyuktaL <sanyukta.thelastmilestone@gmail.com>
2024-04-01 15:06:44 +0530

    sample class added for test

--- src/Common_Components/RoundedImage.jsx
+++ src/Common_Components/RoundedImage.jsx
@@ -4,7 +4,7 @@ import Badge from './Badge';
 const RoundedImage = ({ imageUrls, alt,  isStatusRing = false }) => {
   // console.log(imageUrls,alt,isStatusRing);
   return (
-    <div className='mt-4 ms-4 '>
+    <div className='mt-4 ms-4 border-0'>
       {imageUrls?.map((imageUrl, index) => (
         <div key={index} className="w-12 mt-4 rounded-full ">
           <img  src={imageUrl.src} alt={alt} className={`relative  ${isStatusRing ? 'ring ring-offset-2 ring-[#4F8BED] rounded-full' : ''}`} />
